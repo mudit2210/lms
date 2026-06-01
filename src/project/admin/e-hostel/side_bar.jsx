fdce68b32d364d7575475a78068fdeb59f4e134c
mudit2210 <mudit2210@gmail.com>
2026-06-01 14:14:14 +0530

    name change

--- src/project/admin/e-hostel/side_bar.jsx
+++ src/project/admin/e-hostel/side_bar.jsx
@@ -151,7 +151,7 @@ export default function SideBar({ activeTab, setActiveTab, isSidebarOpen, setIsS
         },
         {
           id: 'resource_management',
-          label: 'Resource Management',
+          label: 'Assests Management',
           icon: (
             <svg className="w-4.5 h-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
